538c915b81283f41b43c60b4f61b5d1a9eda5b45
Paul Kehlhoffner <94603126+polo77k@users.noreply.github.com>
2023-11-19 18:35:29 +0100

    Background GameOver

--- src/space-shooter/scenes/exit.cpp
+++ src/space-shooter/scenes/exit.cpp
@@ -7,6 +7,15 @@ namespace space_shooter
 
   void exitGame(ecs::Manager& manager)
   {
+    // Background
+    int window_width = manager.gameState().width;
+    int window_heigth = manager.gameState().height;
+    auto imagePath = manager.gameState().config.path_to_textures / "GameOver.png";
+    manager.registerEntity<space_shooter::ecs::BackgroundEntity>(imagePath, window_width, window_heigth);
+    
+    // RenderingSystem
+    manager.registerSystem<space_shooter::ecs::RenderingSystem>();
+
     // SceneExit
     manager.registerEntity<space_shooter::ecs::SceneExit>();
     
