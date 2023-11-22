b9915a8ef12e69d3aea84be2e97b454021e33a14
Paul Kehlhoffner <94603126+polo77k@users.noreply.github.com>
2023-11-22 10:31:54 +0100

    correction heatlth bar

--- src/space-shooter/ecs/systems/health_bar_display_system.cpp
+++ src/space-shooter/ecs/systems/health_bar_display_system.cpp
@@ -1,6 +1,7 @@
 #include <space-shooter/ecs/systems/health_bar_display_system.hpp>
 
-#include <space-shooter/ecs/components/position_component.hpp>
+#include <space-shooter/ecs/entities/player_ship.hpp>
+
 #include <space-shooter/ecs/components/sprite_component.hpp>
 #include <space-shooter/ecs/components/tag_component.hpp>
 #include <space-shooter/ecs/components/health_component.hpp>
@@ -16,43 +17,30 @@ namespace space_shooter::ecs {
 
 HealthBarDisplaySystem::HealthBarDisplaySystem()
     : System{
-          type_list<PositionComponent, TagComponent, SpriteComponent>{}} {}
+          type_list<TagComponent, SpriteComponent>{}} {}
 
 void HealthBarDisplaySystem::update(const sf::Time &delta_time,
                             std::vector<Entity *> &entities, Manager &manager) {
 
-  for (auto e : entities) {
+    auto healthShip = manager.getFromEntity<PlayerShipEntity>([](auto &score){return score.get<HealthComponent>();});
+
+    if(!healthShip.has_value())
+    {
+        return;
+    }
+    
+    for (auto e : entities) {
     assert(hasRequiredComponents(*e));
 
     const auto &tag = e->get<TagComponent>();
+    auto &sprite = e->get<SpriteComponent>();
     
     // Tag PlayerShip
-    if(tag.tag == "PlayerShip")
+    if(tag.tag == "PlayerShipHealthBar")
     {
-        const auto &health = e->get<HealthComponent>();
-
-        for (auto e2 : entities)
-        {
-            assert(hasRequiredComponents(*e2));
-
-            const auto &tag2 = e2->get<TagComponent>();
-            auto &sprite2 = e2->get<SpriteComponent>();
-
-            // Tag PlayerShipHealthBar
-            if(tag2.tag == "PlayerShipHealthBar")
-            {   
-                // Display Bar
-                sprite2.width = 1800 * (health.health / health.max_health);
-            }
-        }
-
-
-
+        // Display Bar
+        sprite.width = 1800 * (healthShip.value().health / healthShip.value().max_health);
     }
-
-
-    
-
   }
 }
 
